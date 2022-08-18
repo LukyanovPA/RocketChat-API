59bddc277240ee1849bd5d69a7fc9e079eb01d52
lukyanovpavel <pavel.lukyanovv@gmail.com>
2022-08-18 21:32:36 +0300

    Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,8 +36,6 @@ dependencies {
     implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 
     implementation("org.postgresql:postgresql:$postgresql_version")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
