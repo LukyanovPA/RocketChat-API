89d798e9edf4f6486c295295785cbca7fd8d616e
lukyanovpavel <pavel.lukyanovv@gmail.com>
2022-09-10 21:46:18 +0300

    feat: getAllChatrooms

--- src/main/kotlin/com/pavellukyanov/data/chatrooms/AllChatroomsResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.pavellukyanov.data.chatrooms
-
-import com.pavellukyanov.feature.chatrooms.entity.Chatroom
-
-@kotlinx.serialization.Serializable
-data class AllChatroomsResponse(
-    val chats: List<Chatroom>
-)
